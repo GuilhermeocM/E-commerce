c1ce07c5c02c34fca34c468879ad244e54490020
Guilherme <guilhermeoc.m@gmail.com>
2023-08-22 16:34:58 -0300

    atualizações no model produto

--- produto/models.py
+++ produto/models.py
@@ -3,8 +3,24 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=250)
     descricao_curta = models.TextField(max_length=250)
-    descricao_longa  = models.TextField(max_length=250)
-    imagem  = models.ImageField(upload_to='produto_imagens/Y%/%m/', blank=True, null=True)
+    descricao_longa  = models.TextField()
+    imagem  = models.ImageField(upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
     slug  = models.SlugField(unique=True)
     preco_marketing  = models.FloatField()
     preco_marketing_promocional  = models.FloatField(default=0)
+
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        print(img.name)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+    def __str__(self):
+        return self.nome
